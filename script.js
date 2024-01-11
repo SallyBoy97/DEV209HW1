53a2213181c456f89421a7d7697921d02f9f3cb3
cricht-bc <c.richter@bellevuecollege.edu>
2024-01-10 18:25:57 -0800

    Buttons troubleshooting

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // Initialize an empty array to store words
 var wordsArray = [];
-
+Init.style.display = "visible";
+Result.style.visibility = "hidden";
 // Loop to prompt the user to enter three words
 for (var i = 0; i < 3; i++) {
     // Prompt the user and store the entered word in the array
@@ -28,3 +29,24 @@ for (var j = 0; j < wordsArray.length; j++) {
 // Append the ul element to the body of the document
 document.body.appendChild(headerElement);
 document.body.appendChild(ulElement);
+
+//adding button to initialize swapping/scene change
+/*
+const buttonCheck = document.querySelector('button');
+buttonCheck.addEventListener('click', Swaparoo);
+const swapButton = document.createElement('button');
+swapButton.textContent = 'Swap!';
+document.body.appendChild(swapButton);
+swapButton.addEventListener('click', () => {
+    alert('button clicked!');
+});
+*/
+
+//swapping the first and last letters 
+function Swaparoo (){
+
+    //remove part 1
+    Init.style.display = "hidden";
+	Result.style.visibility = "visible";
+    //word.charAt(word.length - 1) + word.substring(1, word.length - 1) + word.charAt(0);
+}
